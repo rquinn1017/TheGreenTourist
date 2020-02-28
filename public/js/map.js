1767ade236727a296b6e3a60f26abb573a40ee5b
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-02-27 20:06:59 -0500

    removed console logs

--- public/js/map.js
+++ public/js/map.js
@@ -12,7 +12,7 @@ function initMap() {
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-       pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
@@ -41,8 +41,8 @@ function initMap() {
   $.ajax({
     method: "GET",
     url: "api/companies"
-  }).done(function(companies) {
-    console.log(companies);
+  }).done(function (companies) {
+
     let markers = [];
     var infoWindows = [];
 
@@ -50,18 +50,18 @@ function initMap() {
       var lat = companies[i].Latitude;
       var lng = companies[i].Longitude;
       var webpage = companies[i].Website;
-      if (webpage.length>0){
-        if (webpage.substring(0, 4) !== "http"){
+      if (webpage.length > 0) {
+        if (webpage.substring(0, 4) !== "http") {
           webpage = "https://" + webpage
         }
         webpage = `<a href="${webpage}" target="_blank">Visit Website</a>`
       }
-      
+
       infoWindows[i] = new google.maps.InfoWindow({
-        content: companies[i].Facility + `<div>${companies[i].Contact}</div>`  + companies[i].Address + `<br>` +  webpage
-        + `<br>` + `<a href="https://www.google.com/maps/dir/${pos.lat},${pos.lng}/${companies[i].Latitude},${companies[i].Longitude}" target="_blank">Get Directions</a>`
+        content: companies[i].Facility + `<div>${companies[i].Contact}</div>` + companies[i].Address + `<br>` + webpage
+          + `<br>` + `<a href="https://www.google.com/maps/dir/${pos.lat},${pos.lng}/${companies[i].Latitude},${companies[i].Longitude}" target="_blank">Get Directions</a>`
       });
-      console.log(i, lat, lng);
+
       markers[i] = new google.maps.Marker({
         map: map,
         position: {
@@ -70,8 +70,8 @@ function initMap() {
         },
         title: companies[i].Facility
       });
-      markers[i].addListener('click', function(){
-        // console.log(infoWindows);
+      markers[i].addListener('click', function () {
+
         infoWindows[i].open(map, markers[i]);
       });
     }
@@ -91,27 +91,27 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 $.ajax({
   method: "GET",
   url: "api/companies"
-}).done(function(companies) {
-  console.log(companies);
+}).done(function (companies) {
+
 });
 
-$(document).ready(function() {
-  $("table").on("click", "tr", function(event) {
+$(document).ready(function () {
+  $("table").on("click", "tr", function (event) {
     var $headerRow = $(this)
-        .closest("table")
-        .find("thead tr:first"),
+      .closest("table")
+      .find("thead tr:first"),
       $headerRowTds = $headerRow.find("th");
 
     var $row = $(this).closest("tr"),
       $tds = $row.find("td");
 
-    $headerRowTds.each(function(i) {
+    $headerRowTds.each(function (i) {
       // let header = $(this).text();
       let selectedLID = $tds.eq(5).text();
       let selectedLat = $tds.eq(6).text();
       let selectedLon = $tds.eq(7).text();
 
-      console.log(selectedLID, selectedLat, selectedLon);
+
     });
   });
 });
